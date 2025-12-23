31b630eab0146c346d2a20d734ccf3027c4a77e9
N4than3718 <luolixiang5310@gmail.com>
2025-12-23 11:15:11 +0800

    Create ExtractionZone.cs

---
+++ Assets/Scripts/ExtractionZone.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class ExtractionZone : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        // 檢查進入的是不是玩家 (確保你的物品都有 "Player" tag)
+        if (other.CompareTag("Player"))
+        {
+            // 呼叫導演喊卡
+            GameDirector.Instance.TriggerVictory();
+        }
+    }
+}
