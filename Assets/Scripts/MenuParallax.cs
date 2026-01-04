ace26cc00ca387a9ef410e3b7c1f666c60949fdf
N4than3718 <luolixiang5310@gmail.com>
2026-01-05 01:19:51 +0800

    1

--- Assets/Scripts/MenuParallax.cs
+++ Assets/Scripts/MenuParallax.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class MenuParallax : MonoBehaviour
 {
@@ -12,9 +13,11 @@ public class MenuParallax : MonoBehaviour
 
     void Update()
     {
+        Vector2 mousePos = Mouse.current.position.ReadValue();
+
         // 獲取滑鼠在螢幕上的標準化位置 (-1 到 1)
-        float mouseX = (Input.mousePosition.x / Screen.width) - 0.5f;
-        float mouseY = (Input.mousePosition.y / Screen.height) - 0.5f;
+        float mouseX = (mousePos.x / Screen.width) - 0.5f;
+        float mouseY = (mousePos.y / Screen.height) - 0.5f;
 
         // 讓背景往相反方向微動
         transform.position = startPos + new Vector3(mouseX * offsetMultiplier, mouseY * offsetMultiplier, 0);
